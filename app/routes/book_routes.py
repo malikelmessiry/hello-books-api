80e682d3367b41c81268bd43fa95b31d1df71bcf
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-18 17:11:41 +0200

    order of functions

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -16,17 +16,6 @@ def get_all_books():
         )
     return books_response
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        response = {"message":f"book {book_id} invalid"}
-        abort(make_response(response, 400))
-    for book in books:
-        if book.id == book_id:
-            return book
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
@@ -38,3 +27,14 @@ def get_one_book(book_id):
         "description": book.description
     }
         
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message":f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+    for book in books:
+        if book.id == book_id:
+            return book
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
