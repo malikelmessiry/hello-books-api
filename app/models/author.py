34a0846a5ccd01178e20672b3e0de5225797e128
Malik Elmessiry <malik.messiry@gmail.com>
2025-05-06 19:39:39 +0200

    add attributes to parent and child

--- app/models/author.py
+++ app/models/author.py
@@ -1,9 +1,11 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
+from typing import Optional
 
 class Author(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
+    cats: Mapped[Optional[list["Book"]]] = relationship(back_populates="author")b
 
     def to_dict(self):
         author_as_dict = {
